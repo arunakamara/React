86ce4a850ebaaf017c64335053ee4118a9e11d15
arunakamara <kamaraaruna198@gmail.com>
2025-08-02 17:04:36 +0530

    Outsourcing Context and State to a separate Provider Component

--- Elegant Context - Cart/src/App.jsx
+++ Elegant Context - Cart/src/App.jsx
@@ -1,82 +1,16 @@
-import { useState } from "react";
-
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import { CartContext } from "./store/ShoppingCartContext.jsx";
+import CartContextProvider from "./store/ShoppingCartContext.jsx";
 
 function App() {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  const ctxValue = {
-    items: shoppingCart.items,
-    addItemToCart: handleAddItemToCart,
-    UpdateItemQuantity: handleUpdateCartItemQuantity,
-  };
+  
 
   return (
     <>
       {/* In Older React versions (< 19) we use CartContext.Provider instead of just CartContext */}
-      <CartContext value={ctxValue}>
+      <CartContextProvider >
         <Header />
         <Shop>
           {DUMMY_PRODUCTS.map((product) => (
@@ -85,7 +19,7 @@ function App() {
             </li>
           ))}
         </Shop>
-      </CartContext>
+      </CartContextProvider>
     </>
   );
 }
