37c07de4a4024361b553885558c74556f61e26fe
arunakamara <kamaraaruna198@gmail.com>
2025-08-04 15:12:41 +0530

    Implemented Reset button

--- counter/src/components/Counters.jsx
+++ counter/src/components/Counters.jsx
@@ -4,7 +4,7 @@ import Counter from "./Counter";
 export default function Counters() {
   const [counters, setCounters] = useState([
     { id: 1, value: 0 },
-    { id: 2, value: 6 },
+    { id: 2, value: 0 },
     { id: 3, value: 0 },
     { id: 4, value: 0 },
   ]);
@@ -30,8 +30,16 @@ export default function Counters() {
     })
   }
 
+  function handleReset(){
+    setCounters(prevCounters => {
+        const updatedCounters = [ ...prevCounters.map(counter => ({...counter, value: 0})) ]
+        return updatedCounters;
+    })
+  }
+
   return (
     <div>
+        <button onClick={handleReset} className="mx-5 mt-2 bg-green-300 p-2 rounded-md hover:bg-green-400">Reset</button>
       {counters.map((counter) => (
         <Counter
           key={counter.id}
