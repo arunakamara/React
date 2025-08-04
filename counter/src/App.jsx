ab42e229eed6be3d5c670f17498aa91a0aec2fe6
arunakamara <kamaraaruna198@gmail.com>
2025-08-04 17:40:40 +0530

    Outsourcing to Context Component

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,59 +1,13 @@
-import { useState, useContext } from "react";
 import Counters from "./components/Counters";
 import NavBar from "./components/NavBar";
-import { CountersContext } from "./store/CountersContext";
+import CountersContextProvider from "./store/CountersContext";
 import "./App.css";
 
 export default function App() {
-  const [counters, setCounters] = useState([
-    { id: 1, value: 0 },
-    { id: 2, value: 0 },
-    { id: 3, value: 0 },
-    { id: 4, value: 0 },
-  ]);
-
- 
-  function handleIncrement(counterId) {
-    setCounters((prevCounters) => {
-      let counterToUpdate = prevCounters.find((c) => c.id === counterId);
-      const index = prevCounters.indexOf(counterToUpdate);
-      const updatedCounters = [...prevCounters];
-      counterToUpdate = {
-        ...counterToUpdate,
-        value: counterToUpdate.value + 1,
-      };
-      updatedCounters[index] = counterToUpdate;
-      return updatedCounters;
-    });
-  }
-
-  function handleDelete(counterId) {
-    setCounters((prevCounters) => {
-      const updatedCounters = prevCounters.filter((c) => c.id !== counterId);
-      return updatedCounters;
-    });
-  }
-
-  function handleReset() {
-    setCounters((prevCounters) => {
-      const updatedCounters = [
-        ...prevCounters.map((counter) => ({ ...counter, value: 0 })),
-      ];
-      return updatedCounters;
-    });
-  }
-
-  const ctxValue = {
-    counters: counters,
-    incrementCounter: handleIncrement,
-    deleteCounter: handleDelete,
-    resetCounter: handleReset,
-  };
-
   return (
-    <CountersContext value={ctxValue}>
+    <CountersContextProvider>
       <NavBar />
-      <Counters/>
-    </CountersContext>
+      <Counters />
+    </CountersContextProvider>
   );
 }
--- counter/src/components/NavBar.jsx
+++ counter/src/components/NavBar.jsx
@@ -1,16 +1,16 @@
-import { use } from "react"
-import { CountersContext } from "../store/CountersContext"
+import { use } from "react";
+import { CountersContext } from "../store/CountersContext";
 
+export default function NavBar() {
+  const { counters } = use(CountersContext);
+  const counts = counters.filter((c) => c.value !== 0).length;
 
-export default function NavBar(){
-    const {counters} = use(CountersContext)
-    const counts = counters.filter(c => c.value !== 0).length
-
-    
-    return (
-        <nav className="flex gap-5 items-center h-12 bg-stone-300 p-4">
-            <p className="font-bold text-lg">NavBar</p>
-            <div className="size-7 bg-stone-100 rounded-full text-center text-stone-800 ">{counts}</div>
-        </nav>
-    )
+  return (
+    <nav className="flex gap-5 items-center h-12 bg-stone-300 p-4">
+      <p className="font-bold text-lg">NavBar</p>
+      <div className="size-7 bg-stone-100 rounded-full text-center text-stone-800 ">
+        {counts}
+      </div>
+    </nav>
+  );
 }
