c76eb3966aec46e239cd227722a7d96814e2d428
arunakamara <kamaraaruna198@gmail.com>
2025-08-04 14:47:07 +0530

    Single source of truth

--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,24 +1,21 @@
-import { useState } from "react";
 
-export default function Counter() {
-  const [counter, setCounter] = useState({ value: 0 });
+export default function Counter({counter, onIncrement}) {
 
   
   const { value: count } = counter;
+  
 
     const countValue = count === 0 ? 'Zero' : count;
 
   let counterStyle = "size-8  rounded-sm w-fit p-3 text-center leading-[0.5] ";
   counterStyle += count === 0 ? "bg-yellow-400" : "bg-blue-300";
 
-  function handleIncrement(){
-    setCounter(prevCounter => ({value: prevCounter.value + 1}))
-  }
+ 
   return (
     <div className="m-4 flex gap-5 ">
       <div className={counterStyle}>{countValue}</div>
       <button 
-      onClick={handleIncrement}
+      onClick={ onIncrement}
       className="bg-stone-400 px-2 rounded-sm hover:bg-stone-300">
         Increment
       </button>
--- counter/src/components/Counters.jsx
+++ counter/src/components/Counters.jsx
@@ -1,16 +1,37 @@
-import { useState } from "react"
-import Counter from "./Counter"
+import { useState } from "react";
+import Counter from "./Counter";
 
 export default function Counters() {
-    const [counters, setCounters] = useState([
-        {id: 1, value: 0},
-        {id: 2, value: 6},
-        {id: 3, value: 0},
-        {id: 4, value: 0},
-    ])
-    return (
-        <div>
-           {counters.map(counter => <Counter key={counter.id} />)} 
-        </div>
-    )
+  const [counters, setCounters] = useState([
+    { id: 1, value: 0 },
+    { id: 2, value: 6 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 },
+  ]);
+
+  function handleIncrement(counterId) {
+    setCounters((prevCounters) => {
+      let counterToUpdate = prevCounters.find((c) => c.id === counterId);
+      const index = prevCounters.indexOf(counterToUpdate);
+      const updatedCounters = [...prevCounters];
+      counterToUpdate = {
+        ...counterToUpdate,
+        value: counterToUpdate.value + 1,
+      };
+      updatedCounters[index] = counterToUpdate;
+      return updatedCounters;
+    });
+  }
+
+  return (
+    <div>
+      {counters.map((counter) => (
+        <Counter
+          key={counter.id}
+          counter={counter}
+          onIncrement={() => handleIncrement(counter.id)}
+        />
+      ))}
+    </div>
+  );
 }
