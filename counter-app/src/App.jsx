9fada60aec025987163758f1c28a9ec4877d29d2
arunakamara <kamaraaruna198@gmail.com>
2025-08-06 07:00:06 +0530

    With useState

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,16 +1,36 @@
-
-export function counterReducer() {}
+import { useReducer, useState } from "react";
+export function counterReducer(state, action) {
+  return state;
+}
 
 function App() {
+  // const [counterState, counterDispatch] = useReducer(counterReducer, {
+  //   count: 0,
+  // })
+
+  const [counter, setCounter] = useState({ count: 0})
+
+  function handleIncrement(){
+    setCounter((prevCounter) => ({...prevCounter, count: prevCounter.count + 1}))
+  }
+
+  function handleDecrement(){
+    setCounter(prevCounter => ({...prevCounter, count: prevCounter.count > 0 ? prevCounter.count - 1 : 0}))
+  }
+
+  function handleReset() {
+    setCounter(prevCounter => ({...prevCounter, count: 0}))
+  }
+
   return (
     <div id="app">
       <h1>The (Final?) Counter</h1>
       <p id="actions">
-        <button>Increment</button>
-        <button>Decrement</button>
-        <button>Reset</button>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleReset}>Reset</button>
       </p>
-      <p id="counter"></p>
+      <p id="counter">{counter.count}</p>
     </div>
   );
 }
