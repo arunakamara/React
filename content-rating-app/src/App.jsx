252d6fb84bd6f5f7d53cec3caf6f07a9254ab561
arunakamara <kamaraaruna198@gmail.com>
2025-08-14 15:58:14 +0530

    Likes and Dislikes button

--- content-rating-app/src/App.jsx
+++ content-rating-app/src/App.jsx
@@ -1,21 +1,46 @@
-import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa'
+import { useState } from "react";
+import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
 
-export default function App(){
+export default function App() {
+  const [rating, setRating] = useState({
+    likes: 0,
+    dislikes: 0,
+  });
+
+  function handleLike() {
+    setRating((prevRating) => ({ ...prevRating, likes: prevRating.likes + 1 }));
+  }
+
+  function handleDislike() {
+    setRating((prevRating) => ({
+      ...prevRating,
+      dislikes: prevRating.dislikes + 1,
+    }));
+  }
+  
+  const { likes, dislikes } = rating;
+
   return (
     <>
       <div className="flex flex-col justify-center items-center mt-30 bg-stone-300 w-80 h-50 m-auto rounded-lg">
         <p className="font-bold leading-8 mt-5">Text</p>
-        <menu className='flex mt-20 gap-5'>
-          <button className='flex gap-2 mx-2 bg-stone-700 text-stone-200 p-3 rounded-lg hover:bg-stone-500'>
-            Likes <FaThumbsUp className='text-blue-400 mt-1'/>
-             {0}
-            </button>
-          <button className='flex gap-2 mx-2 bg-stone-700 text-stone-200 p-3 rounded-lg hover:bg-stone-500'>
-            Dislike <FaThumbsDown className='text-blue-400 mt-1 '/>
-             {0}
-            </button>
+        <menu className="flex mt-20 gap-5">
+          <button
+            onClick={handleLike}
+            className="flex gap-2 mx-2 bg-stone-700 text-stone-200 p-3 rounded-lg hover:bg-stone-500"
+          >
+            Likes <FaThumbsUp className="text-blue-400 mt-1" />
+            ({likes})
+          </button>
+          <button
+            onClick={handleDislike}
+            className="flex gap-2 mx-2 bg-stone-700 text-stone-200 p-3 rounded-lg hover:bg-stone-500"
+          >
+            Dislike <FaThumbsDown className="text-blue-400 mt-1 " />
+            ({dislikes})
+          </button>
         </menu>
       </div>
     </>
-  )
+  );
 }
