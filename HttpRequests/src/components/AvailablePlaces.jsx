566a7c4c579397c120bc0977de1c6d365b7b37f4
arunakamara <kamaraaruna198@gmail.com>
2025-08-23 17:21:16 +0530

    Using await/async with axios

--- HttpRequests/src/components/AvailablePlaces.jsx
+++ HttpRequests/src/components/AvailablePlaces.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import axios from 'axios'
 
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces ] = useState([])
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-    .then(res => res.json())
-    .then(data => setAvailablePlaces(data.places));
+    const fetchPlaces = async () => {
+      const {data} = await axios.get("http://localhost:3000/places")
+      setAvailablePlaces(data.places)
+    }
+    fetchPlaces();
+    // fetch("http://localhost:3000/places")
+    // .then(res => res.json())
+    // .then(data => setAvailablePlaces(data.places));
   }, [])
 
 
