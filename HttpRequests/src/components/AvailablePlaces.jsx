1d073e72b4f91c72a8eff964099eed75a4710ed7
arunakamara <kamaraaruna198@gmail.com>
2025-08-23 16:52:34 +0530

    Fetching data and useEffect

--- HttpRequests/src/components/AvailablePlaces.jsx
+++ HttpRequests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,22 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
+
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces ] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+    .then(res => res.json())
+    .then(data => setAvailablePlaces(data.places));
+  }, [])
+
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
