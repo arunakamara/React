187d73a92a9c76f8c7acfbb6529da33bc30fb2aa
arunakamara <kamaraaruna198@gmail.com>
2025-08-23 19:15:55 +0530

    Error Handling

--- HttpRequests/src/components/AvailablePlaces.jsx
+++ HttpRequests/src/components/AvailablePlaces.jsx
@@ -1,28 +1,46 @@
-import { useState, useEffect } from 'react';
-import Places from './Places.jsx';
-import axios from 'axios'
-
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
+import axios from "axios";
+import ErrorPage from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces ] = useState([])
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    const fetchPlaces = async () => {
-      const {data} = await axios.get("http://localhost:3000/places")
-      setAvailablePlaces(data.places)
+
+
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places.");
+        }
+      setAvailablePlaces(resData.places);
+
+      } catch (error) {
+        setError(error);
+      }
+
+      setIsFetching(false);
     }
     fetchPlaces();
-    // fetch("http://localhost:3000/places")
-    // .then(res => res.json())
-    // .then(data => setAvailablePlaces(data.places));
-  }, [])
+  }, []);
 
-
+  if (error) {
+    return <ErrorPage title="An error occurred!" message={error.message}  />
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
