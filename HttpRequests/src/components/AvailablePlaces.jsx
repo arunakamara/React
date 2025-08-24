f9001b9a68de54e0a029f3335a435628be6f6d05
arunakamara <kamaraaruna198@gmail.com>
2025-08-24 21:32:50 +0530

    Custom Hooks in Multiple Components

--- HttpRequests/src/components/AvailablePlaces.jsx
+++ HttpRequests/src/components/AvailablePlaces.jsx
@@ -3,36 +3,32 @@ import Places from "./Places.jsx";
 import ErrorPage from "./Error.jsx";
 import { sortPlacesByDistance } from "./../loc";
 import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../hooks/UseFetch.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [availablePlaces, setAvailablePlaces] = useState([]);
+  // const [error, setError] = useState();
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        
-        const places = await fetchAvailablePlaces();
+  const {
+    isFetching,
+    fetchedData: availablePlaces,
+    error,
+    setFetchedData: setAvailablePlaces,
+  } = useFetch(fetchAvailablePlaces, []);
 
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
+  // navigator.geolocation.getCurrentPosition((position) => {
+  //   const sortedPlaces = sortPlacesByDistance(
+  //     places,
+  //     position.coords.latitude,
+  //     position.coords.longitude
+  //   );
 
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError(error);
-        setIsFetching(false);
-      }
-    }
-    fetchPlaces();
-  }, []);
+  //   setAvailablePlaces(sortedPlaces);
+  //   setIsFetching(false);
+  // });
+
+
 
   if (error) {
     return <ErrorPage title="An error occurred!" message={error.message} />;
