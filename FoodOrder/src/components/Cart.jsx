a90fd323a008cc1819f148ae9c10c62f0135bc83
arunakamara <kamaraaruna198@gmail.com>
2025-09-19 19:29:49 +0530

    Increase and Decrease buttons

---
+++ FoodOrder/src/components/Cart.jsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "./../util/formatting";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
+
+export default function Cart() {
+  const { items, addItem, removeItem } = useContext(CartContext);
+  const { progress, hideCart } = useContext(UserProgressContext);
+
+  const cartTotal = items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleCloseCart() {
+    hideCart();
+  }
+
+  return (
+    <Modal className="cart" open={progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onDecrease={() => removeItem(item.id)}
+            onIncrease={() => addItem(item)}
+          />
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
--- FoodOrder/src/components/Header.jsx
+++ FoodOrder/src/components/Header.jsx
@@ -2,12 +2,19 @@ import { useContext } from "react";
 import CartContext from "../store/CartContext";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+
 export default function Header() {
   const { items } = useContext(CartContext);
+  const { showCart } = useContext(UserProgressContext);
 
   const totalCartItems = items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    showCart();
+  }
 
   return (
     <header id="main-header">
@@ -16,7 +23,9 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
