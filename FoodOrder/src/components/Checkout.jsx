ac1a6bca975a877760c1cf3361a6d9359a11b405
arunakamara <kamaraaruna198@gmail.com>
2025-09-19 21:52:51 +0530

    sending a post request

--- FoodOrder/src/components/Checkout.jsx
+++ FoodOrder/src/components/Checkout.jsx
@@ -23,6 +23,19 @@ export default function Checkout() {
 
         const fd = new FormData(e.target);
         const customerData = Object.fromEntries(fd.entries());
+
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items,
+                    customer: customerData
+                }
+            })
+        })
     }
 
     return <Modal open={progress === 'checkout'} onClose={handleClose}>
@@ -30,7 +43,7 @@ export default function Checkout() {
             <h2>Checkout</h2>
             <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-            <Input label="Full Name" type="text" id="full-name"/>
+            <Input label="Full Name" type="text" id="name"/>
             <Input label="E-Mail Address" type="email" id="email"/>
             <Input label="Street Address" type="text" id="street"/>
 
