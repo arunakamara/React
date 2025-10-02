4672c2b5694edc2352ea8a1b7785b4a70d0c3a4e
arunakamara <kamaraaruna198@gmail.com>
2025-10-02 23:24:10 +0530

    Footer

--- testRedux/test-redux/src/components/Footer.jsx
+++ testRedux/test-redux/src/components/Footer.jsx
@@ -1,9 +1,17 @@
 const Footer = () => {
-    return ( 
-        <>
-        <h1>Footer</h1>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <footer className="text-stone-100 text-sm flex justify-between items-center">
+        <address >123 Street, City, Country <br/> Email: info@example.com</address>
+        <p className="text-sm self-end mb-1 text-stone-300">© <i>2025 Brain.</i> All rights reserved.</p>
+        <nav className="text-stone-100 flex gap-2">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </nav>
+      </footer>
+    </>
+  );
+};
+
 export default Footer;
--- testRedux/test-redux/src/components/Header.jsx
+++ testRedux/test-redux/src/components/Header.jsx
@@ -1,16 +1,16 @@
-
 export default function Header() {
-
-    return <>
-    <header className="flex justify-between align-center p-2">
+  return (
+    <>
+      <header className="flex justify-between align-center p-2">
         <h2 className="text-2xl text-stone-50 font-bold ">Welcome USER</h2>
         <nav>
-            <ul className="flex gap-2 text-white font-bold">
-                <li>Orders</li>
-                <li>Cart</li>
-                <li>Account</li>
-            </ul>
+          <ul className="flex gap-2 text-white font-bold">
+            <li>Orders</li>
+            <li>Cart</li>
+            <li>Account</li>
+          </ul>
         </nav>
-    </header>
+      </header>
     </>
+  );
 }
