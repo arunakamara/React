558958326f6f1567d289df0fe8ce257aad78b2de
arunakamara <kamaraaruna198@gmail.com>
2025-10-02 23:01:22 +0530

    Header

--- testRedux/test-redux/src/components/Header.jsx
+++ testRedux/test-redux/src/components/Header.jsx
@@ -2,6 +2,15 @@
 export default function Header() {
 
     return <>
-    <h1>Header</h1>
+    <header className="flex justify-between align-center p-2">
+        <h2 className="text-2xl text-stone-50 font-bold ">Welcome USER</h2>
+        <nav>
+            <ul className="flex gap-2 text-white font-bold">
+                <li>Orders</li>
+                <li>Cart</li>
+                <li>Account</li>
+            </ul>
+        </nav>
+    </header>
     </>
 }
