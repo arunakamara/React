c63183310a6cfd896b59c1aaaa28fe1ff1373e29
arunakamara <kamaraaruna198@gmail.com>
2025-10-02 22:35:59 +0530

    Initial commit

---
+++ testRedux/test-redux/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Auth />
+      <UserProfile />
+      <Footer />
+    </>
+  );
+}
